1d2daf52819b250dd26e129b3eb350deb018b25e
josemlegal <josemlegal97@gmail.com>
2022-12-13 15:05:55 -0300

    added date format

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
 
 void main() => runApp(const MyApp());
@@ -85,7 +86,7 @@ class MyHomePage extends StatelessWidget {
                           style: TextStyle(
                               fontSize: 16, fontWeight: FontWeight.bold),
                         ),
-                        Text(tx.date.toString(),
+                        Text(DateFormat('dd-MM-yyyy').format(tx.date),
                             style: TextStyle(color: Colors.grey)),
                       ],
                     ),
